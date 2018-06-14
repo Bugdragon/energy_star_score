dadc902c4518af1b72a9ddd087b1edc5c8cc5d78
Baolong Niu <465002300@qq.com>
2018-06-15 01:21:56 +0800

    Add files via upload

---
+++ data_explore.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('./data/Energy_and_Water_Data_Disclosure_for_Local_Law_84_2017__Data_for_Calendar_Year_2016_.csv')
+#print(data.head()) # 60列
+#print(data.info())
+
+data = data.replace({'Not Available': np.nan}) # np.nan可解释为数字
+for column in list(data.columns):
+    # 选择应为数字的列
+    if('ft²' in column or 'kBtu' in column or 'Metric Tons CO2e' in column
+            or 'kWh' in column or 'therms' in column or 'gal' in column or 'Score' in column):
+        data[column] = data[column].astype(float) # 转换类型为float
+
+# 数据探索,describe()函数
+result = './data/describe.xls'
+explore = data.describe().T
+explore['null'] = len(data) - explore['count']
+explore['null(%)'] = explore['null'] / len(data) * 100
+explore = explore[['null', 'max', 'min', 'null(%)']]
+explore.columns = [u'缺失值', u'最大值', u'最小值', u'缺失值占比']
+explore.to_excel(result)
+
+
